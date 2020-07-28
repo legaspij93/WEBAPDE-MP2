609f6448b60b94864d9cc5973d501035f57a9345
legaspij93 <47266085+legaspij93@users.noreply.github.com>
2020-07-29 01:49:15 +0800

    unit tests added for BE_DEV-02

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,20 @@ const urlencoder = bodyparser.urlencoded({
 
 router.use(urlencoder)
 
+function validation(user){
+    if(user.email && user.password){
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    else{
+        return false
+    }
+}
+
 router.post("/register", function(req, res){
     var user = {
         username : req.body.username,
@@ -30,17 +44,22 @@ router.post("/login", function(req, res){
         email: req.body.email,
         password: req.body.password
     }
-    
-    User.authenticate(user).then((newUser)=>{
-        if(newUser){
-            req.session.email = user.email
-            console.log(req.session.email)
-            res.redirect("/game/games")
-            // res.render("dashboard.hbs")
-        }
-    }, (error)=>{
-        res.sendFile(error)
-    })
+    if(validation(user)){
+        User.authenticate(user).then((newUser)=>{
+            if(newUser){
+                req.session.email = user.email
+                console.log(req.session.email)
+                res.redirect("/game/games")
+                // res.render("dashboard.hbs")
+            }
+        }, (error)=>{
+            res.sendFile(error)
+        })
+    }
+    else{
+        //insert error message here
+        res.redirect("/loginpage")
+    }
 })
 
 router.get("/loginpage", function(req,res){
