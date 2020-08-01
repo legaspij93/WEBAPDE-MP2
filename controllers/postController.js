35a15c01a76340b0ea3696678d622d52e2570735
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2020-08-01 18:02:00 +0800

    finished error messages for game listings

--- controllers/postController.js
+++ controllers/postController.js
@@ -43,14 +43,25 @@ router.post("/new-post", function(req, res){
         
          if(listingValidation(post)){
             Post.create(post).then((post)=>{
-                res.redirect("upload")
+                res.redirect("/game/games")
             }, (error)=>{
                 res.sendFile(error)
             })
         }
         else{
-            //insert error message
-            console.log("error")
+            console.log(post)
+            req.session.errors = []
+            if(!post.title)
+                req.session.errors.push({"container-id": "title","message": "Game is not selected"})
+            if(post.price < 0 || post.price > 10000)
+                req.session.errors.push({"container-id": "price","message": "Invalid price value, must be between 0 and 10000"})
+            if(post.description == '')
+                req.session.errors.push({"container-id": "description","message": "Condition must be filled"})
+            
+            req.session.savedinput = [{"container-id": "price", "content": post.price}, 
+                                      {"container-id": "description", "content": post.description}]
+            if(req.body.title)
+                req.session.savedinput.push({"container-id": "title", "content": post.title})
             res.redirect("upload")
         }
     })
@@ -61,10 +72,14 @@ router.get("/available", function(req, res){
 })
 
 router.get("/upload", function(req, res){
+    var errors = req.session.errors
+    var savedinput = req.session.savedinput
+    req.session.errors = null
+    req.session.savedinput = null
     Game.getAll().then((games)=>{
         User.getUser(req.session.email).then((user)=>{
             res.render("upload.hbs", {
-                games, user
+                games, user, errors, savedinput
             })
         })
     })
